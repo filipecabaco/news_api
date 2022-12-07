23f0b179c1b20c64e2bbe2178b2ca6996b83ec73
Filipe Cabaco <filipe@supabase.io>
2022-12-07 00:13:40 +0000

    set error handling

--- lib/router.ex
+++ lib/router.ex
@@ -1,13 +1,21 @@
 defmodule NewsApi.Router do
   use Plug.Router
+  use Plug.ErrorHandler
+
   plug(:match)
   plug(:dispatch)
 
   get "/" do
+    raise "Potato"
     send_resp(conn, 200, "howdy")
   end
 
   match _ do
-    send_resp(conn, 404, "Not found")
+    send_resp(conn, 404, "not found")
+  end
+
+  @impl Plug.ErrorHandler
+  def handle_errors(conn, _) do
+    send_resp(conn, conn.status, "internal server error")
   end
 end
